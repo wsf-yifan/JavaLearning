3e77aa581805ceb2c49adaca17db610e5e6ae2c0
Shaofan Wang <v-shaofw@microsoft.com>
2020-03-24 15:52:43 +0800

    添加swagger

--- SpringBoot/src/main/java/com/yifan/springboot/DemoApplication.java
+++ SpringBoot/src/main/java/com/yifan/springboot/DemoApplication.java
@@ -1,51 +1,51 @@
 package com.yifan.springboot;
 
-import com.yifan.springboot.aop.AopConfig;
-import com.yifan.springboot.aop.DemoAnnotationService;
-import com.yifan.springboot.aop.DemoMethodService;
-import com.yifan.springboot.config.DiConfig;
-import com.yifan.springboot.config.JavaConfig;
-import com.yifan.springboot.service.UseFunctionService;
-import com.yifan.springboot.service.UseHelloService;
+// import com.yifan.springboot.aop.AopConfig;
+// import com.yifan.springboot.aop.DemoAnnotationService;
+// import com.yifan.springboot.aop.DemoMethodService;
+// import com.yifan.springboot.config.DiConfig;
+// import com.yifan.springboot.config.JavaConfig;
+// import com.yifan.springboot.service.UseFunctionService;
+// import com.yifan.springboot.service.UseHelloService;
 
-// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+// import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 
-		System.out.println("Begin to start.");
+		// System.out.println("Begin to start.");
 
-		// normal configuration
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
+		// // normal configuration
+		// AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
 
-		UseHelloService useHelloService = context.getBean(UseHelloService.class);
-		System.out.println(useHelloService.sayHello());
+		// UseHelloService useHelloService = context.getBean(UseHelloService.class);
+		// System.out.println(useHelloService.sayHello());
 
-		context.close();
+		// context.close();
 
-		// JAVA CONFIGURATION
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JavaConfig.class);
-		UseFunctionService useFunctionService = ctx.getBean(UseFunctionService.class);
+		// // JAVA CONFIGURATION
+		// AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JavaConfig.class);
+		// UseFunctionService useFunctionService = ctx.getBean(UseFunctionService.class);
 
-		System.out.println(useFunctionService.sayHi());
-		ctx.close();
+		// System.out.println(useFunctionService.sayHi());
+		// ctx.close();
 
-		System.out.println("AOP使用");
+		// System.out.println("AOP使用");
 
-		AnnotationConfigApplicationContext aop = new AnnotationConfigApplicationContext(AopConfig.class);
-		DemoAnnotationService demoAnnotationService = aop.getBean(DemoAnnotationService.class);
-		DemoMethodService demoMethodService = aop.getBean(DemoMethodService.class);
+		// AnnotationConfigApplicationContext aop = new AnnotationConfigApplicationContext(AopConfig.class);
+		// DemoAnnotationService demoAnnotationService = aop.getBean(DemoAnnotationService.class);
+		// DemoMethodService demoMethodService = aop.getBean(DemoMethodService.class);
 
-		//invoke methods
-		demoAnnotationService.add();
-		demoMethodService.add();
+		// //invoke methods
+		// demoAnnotationService.add();
+		// demoMethodService.add();
 
-		aop.close();
+		// aop.close();
 	}
 
 }
--- SpringBoot/src/main/java/com/yifan/springboot/controller/HelloController.java
+++ SpringBoot/src/main/java/com/yifan/springboot/controller/HelloController.java
@@ -3,10 +3,21 @@ package com.yifan.springboot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value = "测试用的Hello controller", tags="USED FOR TEST")
 public class HelloController{
+    @ApiOperation("hello测试")
     @GetMapping("/hello")
     public String hello(){
         return "Hello Spring Again.";
+    }
+
+    @ApiOperation("hi测试")
+    @GetMapping("/hi")
+    public String hi(){
+        return "Hi Spring Again.";
     }
 }
