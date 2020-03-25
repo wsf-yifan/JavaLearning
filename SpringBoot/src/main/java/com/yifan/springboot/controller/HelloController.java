d82ef26220187c845fc59ad202dfd0073f33a52b
wyifan <wyifan@vip.qq.com>
2020-03-25 16:09:12 +0800

    添加全局api支持

--- SpringBoot/src/main/java/com/yifan/springboot/controller/HelloController.java
+++ SpringBoot/src/main/java/com/yifan/springboot/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.yifan.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@RequestMapping("/api")
 @Api(value = "测试用的Hello controller", tags="USED FOR TEST")
 public class HelloController{
     @ApiOperation("hello测试")
