40b726d5e425d63ffea03e0285eccf08e9d4ec05
wyifan <wyifan@vip.qq.com>
2020-03-26 08:03:17 +0800

    添加异常处理类

--- SpringBoot/src/main/java/com/yifan/springboot/exception/GlobalExceptionHandler.java
+++ SpringBoot/src/main/java/com/yifan/springboot/exception/GlobalExceptionHandler.java
@@ -14,11 +14,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        System.out.println("IN GLOBAL EXCEPTION CLASS");
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+    public ResponseEntity<?> globeExceptionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
